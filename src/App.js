f01bd43412341ec2f5a2b4f223beb2d7e21cdd17
katemikh <cooperation.ca@yahoo.com>
2023-09-29 16:23:35 -0400

    updated to original file info

--- src/App.js
+++ src/App.js
@@ -1,91 +1,20 @@
-//#1
 import React from "react";
 
 function App() {
-  const todoList = [
-    { id: 1, title: "Complete assignment 1" },
-    { id: 2, title: "Go for a run" },
-    { id: 3, title: "Read React book" },
-  ];
-
-  // the style
-  const listItemStyle = {
-    listStyle: "none", // Removing list bullet points
-  };
-
   return (
-    <div style={{ textAlign: "center" }}>
-      <h1>Todo List</h1>
-      <ul>
-        {todoList.map((todo) => (
-          <li key={todo.id} style={listItemStyle}>
-            {todo.title}
-          </li>
-        ))}
-      </ul>
+    <div
+      style={{
+        textAlign: "center",
+      }}
+    >
+      <header>
+        <p>
+          Edit <code> src / App.js </code> and save to reload.{" "}
+        </p>{" "}
+        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React{" "}
+        </a>{" "}
+      </header>{" "}
     </div>
   );
 }
-
-export default App;
-
-//#2
-// import React from "react";
-
-//   const todoList = [
-//     { id: 1, title: "Complete assignment 1" },
-//     { id: 2, title: "Go for a run" },
-//     { id: 3, title: "Read React book" },
-//   ];
-
-//   // the styles
-//   const listItemStyle = {
-//     listStyle: "none",
-//   };
-
-//function App() {
-//   return (
-//     <div style={{ textAlign: "center" }}>
-//       <h1>Todo List</h1>
-//       <ul>
-//         {todoList.map(function (todo) {
-//           return <li key={todo.id} style={listItemStyle}>
-//             {todo.title}
-//           </li>;
-//         })}
-//       </ul>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-//#3
-// import React from "react";
-
-//   const todoList = [
-//     { id: 1, title: "Complete assignment 1" },
-//     { id: 2, title: "Go for a run" },
-//     { id: 3, title: "Read React book" },
-//   ];
-
-//   function listTodo(todo) {
-//     return <li>{todo.title} </li>;
-//   }
-
-//   // the styles
-//   const listItemStyle = {
-//     listStyle: "none",
-//   };
-// function App() {
-//   return (
-//     <div style={{ textAlign: "center" }}>
-//       <h1>Todo List</h1>
-//       <ul key={todoList.id} style={listItemStyle}>
-//         {todoList.map(listTodo)}
-//       </ul>
-//     </div>
-//   );
-// }
-
-// export default App;
