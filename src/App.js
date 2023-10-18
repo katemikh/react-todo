c4982ad8e85a4e58cbef77a52bbf05c7c9fa9db4
katemikh <cooperation.ca@yahoo.com>
2023-10-17 23:42:37 -0400

    React-Lesson_1.3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import TodoList from "./TodoList";
-import AddTodoForm from "./AddTodoForms";
+import AddTodoForm from "./AddTodoForm";
 
 function App() {
+  const [newTodo, setNewTodo] = useState("");
+
   return (
     <div style={{ textAlign: "center" }}>
       <h1>Todo List</h1>
-        <AddTodoForm />
-        <TodoList />
+      <AddTodoForm  onAddTodo={(todoTitle) => setNewTodo(todoTitle)}/>
+      <p>New Todo: {newTodo}</p> {/* Display the value of newTodo variable */}
+      <TodoList />
     </div>
   );
 }
